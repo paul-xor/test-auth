3bb24d735895969c808369d77d8a6f7d45377d5f
Pavel Smolov <plexsmol@gmail.com>
2022-08-31 17:54:27 -0400

    feat: add data validation

--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class UserRegisterDto {
+	@IsEmail({}, { message: 'Invalid email' })
 	email: string;
+
+	@IsString({ message: 'No password provided' })
 	password: string;
+
+	@IsString({ message: 'No name provided' })
 	name: string;
 }
